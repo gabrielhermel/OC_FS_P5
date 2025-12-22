d36223f3b8b5c84340acb7a3b533a4b245947352
Gabriel Hermel <gabrieljonathanhermel@gmail.com>
2025-12-22 11:11:56 +0100

    test(session-service): add login state emission test

--- Testez-une-application-full-stack/front/src/app/services/session.service.spec.ts
+++ Testez-une-application-full-stack/front/src/app/services/session.service.spec.ts
@@ -2,9 +2,21 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
+
+  // Shared mock information
+  const mockSessionInfo: SessionInformation = {
+    id: 1,
+    admin: true,
+    token: 'abc123',
+    type: 'Bearer',
+    username: 'jdoe',
+    firstName: 'John',
+    lastName: 'Doe',
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,5 +25,19 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // Verify that logging in sets session info and emits logged-in state
+  it('should log in the user and emit true', (done) => {
+    const sub = service.$isLogged().subscribe((value) => {
+      if (value === true) {
+        expect(service.sessionInformation).toEqual(mockSessionInfo);
+        expect(service.isLogged).toBe(true);
+        sub.unsubscribe();
+        done();
+      }
+    });
+
+    service.logIn(mockSessionInfo);
   });
 });

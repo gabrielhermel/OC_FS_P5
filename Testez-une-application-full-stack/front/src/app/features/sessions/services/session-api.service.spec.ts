41ce072f256ed7177695f3577f2d66040f582ce6
Gabriel Hermel <gabrieljonathanhermel@gmail.com>
2025-12-29 14:07:50 +0100

    test(session-api): add all() request test

--- Testez-une-application-full-stack/front/src/app/features/sessions/services/session-api.service.spec.ts
+++ Testez-une-application-full-stack/front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -1,22 +1,56 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionApiService } from './session-api.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Session } from '../interfaces/session.interface';
 
-describe('SessionsService', () => {
+describe('SessionApiService', () => {
   let service: SessionApiService;
+  let httpMock: HttpTestingController;
+
+  // Shared mock information
+  const mockSessionId = 1;
+  const mockSession: Session = {
+    id: mockSessionId,
+    name: 'Yoga Session',
+    description: 'A yoga session',
+    date: new Date('2025-01-01'),
+    teacher_id: 1,
+    users: [],
+    createdAt: new Date('2025-01-01'),
+    updatedAt: new Date('2025-01-02'),
+  };
+  const mockSessions: Session[] = [mockSession];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(SessionApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // Verify sessions list is fetched
+  it('should fetch all sessions', () => {
+    service.all().subscribe((sessions) => {
+      expect(sessions).toEqual(mockSessions);
+    });
+
+    const req = httpMock.expectOne('api/session');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockSessions);
   });
 });
